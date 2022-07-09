fe3d68b4c9a5977823aa3c399450c0f25e4b82a5
Chandler <chandlerawarren@gmail.com>
2022-07-09 17:23:56 -0400

    index.html

--- email-playground/main.py
+++ email-playground/main.py
@@ -2,8 +2,26 @@ from dotenv import load_dotenv
 import os
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
 # loading .env
-load_dotenv('../.env')
-trying = os.environ.get('APP_PASS')
-print(trying)
+load_dotenv()
+app_pass = os.environ.get('APP_PASS')
+
+html = Template(Path('index.html').read_text())
+
+email = EmailMessage()
+email['from'] = 'Chandler Warren'
+email['to'] = 'chandlerawarren@gmail.com'
+email['subject'] = 'You won a bunch of money'
+
+# can use dict for substitute if using multiple variables
+email.set_content(html.substitute(name='Tintin'))
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('testingpython12346@gmail.com', app_pass)
+    smtp.send_message(email)
+    print('all good boss')
